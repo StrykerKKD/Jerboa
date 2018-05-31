54f10b32a1d06aa57831e11b877fbeae8890a87e
Tóth Róbert <kkdstryker@gmail.com>
2018-05-31 21:27:20 +0200

    Remove unneceseary stuff.

--- main.ml
+++ main.ml
@@ -224,100 +224,24 @@ let server handler_config =
     let parameters = capture_parameters uri_handler uri_parts in
     let method_handler = get_method_handler method_handler_config in
     let query = Uri.query uri in
-    let headers =  Request.headers req in
+    (*let headers =  Request.headers req in*)
     Cohttp_lwt.Body.to_string body >>= fun body ->
     let response = apply_method_handler meth method_handler parameters query body in
     send_response response
   in
   Server.create ~mode:(`TCP (`Port 8000)) (Server.make ~callback ())
 
-(*let () = ignore (Lwt_main.run server)*)
+let my_config = {
+  connect_handlers = [];
+  delete_handlers = [];
+  get_handlers = [];
+  head_handlers = [];
+  options_handlers = [];
+  other_handlers = [];
+  patch_handlers = [];
+  post_handlers = [];
+  put_handlers = [];
+  trace_handlers = [];
+}
 
-(*let server =
-  let callback _conn req body =
-
-    let uri = req |> Request.uri |> Uri.to_string in
-    let meth = req |> Request.meth |> Code.string_of_method in
-    let headers = req |> Request.headers |> Header.to_string in
-    body |> Cohttp_lwt.Body.to_string >|= (fun body ->
-        (Printf.sprintf "Uri: %s\nMethod: %s\nHeaders\nHeaders: %s\nBody: %s"
-           uri meth headers body))
-    >>= (fun body -> Server.respond_string ~status:`OK ~body ())
-  in
-  Server.create ~mode:(`TCP (`Port 8000)) (Server.make ~callback ())
-
-  let () = ignore (Lwt_main.run server)*)
-
-(*
-type nonrec status_code =                                                                                            ──┴────────────┴───────────────────┴─────────────┴───────────────┴───────────────────┴───────────────────────┴─────────────────┘
-    [ `Accepted
-    | `Already_reported
-    | `Bad_gateway
-    | `Bad_request
-    | `Bandwidth_limit_exceeded
-    | `Blocked_by_windows_parental_controls
-    | `Checkpoint
-    | `Client_closed_request
-    | `Code of int
-    | `Conflict
-    | `Continue
-    | `Created
-    | `Enhance_your_calm
-    | `Expectation_failed
-    | `Failed_dependency
-    | `Forbidden
-    | `Found
-    | `Gateway_timeout
-    | `Gone
-    | `Http_version_not_supported
-    | `I_m_a_teapot
-    | `Im_used
-    | `Insufficient_storage
-    | `Internal_server_error
-    | `Length_required
-    | `Locked
-    | `Loop_detected
-    | `Method_not_allowed
-    | `Moved_permanently
-    | `Multi_status
-    | `Multiple_choices
-    | `Network_authentication_required
-    | `Network_connect_timeout_error
-    | `Network_read_timeout_error
-    | `No_content
-    | `No_response
-    | `Non_authoritative_information
-    | `Not_acceptable
-    | `Not_extended
-    | `Not_found
-    | `Not_implemented
-    | `Not_modified
-    | `OK
-    | `Partial_content
-    | `Payment_required
-    | `Precondition_failed
-    | `Precondition_required
-    | `Processing
-    | `Proxy_authentication_required
-    | `Request_entity_too_large
-    | `Request_header_fields_too_large
-    | `Request_timeout
-    | `Request_uri_too_long
-    | `Requested_range_not_satisfiable
-    | `Reset_content
-    | `Resume_incomplete
-    | `Retry_with
-    | `See_other
-    | `Service_unavailable
-    | `Switch_proxy
-    | `Switching_protocols
-    | `Temporary_redirect
-    | `Too_many_requests
-    | `Unauthorized
-    | `Unprocessable_entity
-    | `Unsupported_media_type
-    | `Upgrade_required
-    | `Use_proxy
-    | `Variant_also_negotiates
-    | `Wrong_exchange_server ]
-*)
+let () = ignore (Lwt_main.run (server my_config))
