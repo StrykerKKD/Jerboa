c63d2347d234f5d7e50c3f6a52dc87ac45399f50
Tóth Róbert <kkdstryker@gmail.com>
2018-06-26 21:55:43 +0200

    Adding simple server.

--- jerboa.ml
+++ jerboa.ml
@@ -1,3 +1,5 @@
+open Lwt.Infix
+
 type middleware = Request.t -> Request.t
 
 type handler = Request.t -> Response.t
@@ -33,3 +35,15 @@ let find_route_handler_config_by_route route handler_config =
     let compiled_uri_handler = Re.compile composed_uri_handler in
     Re.execp compiled_uri_handler route
   )
+
+let server handler_config =
+  let callback _conn req body =
+    let uri = Request.uri req in
+    let path = Uri.to_string uri in
+    let meth = Request.meth req in
+    let headers =  Request.headers req in
+    Cohttp_lwt.Body.to_string body >>= fun body ->
+    let status = Cohttp.Code.status_of_code 404 in
+    Cohttp_lwt_unix.Server.respond_string ~status ~body ()
+  in
+  Cohttp_lwt_unix.Server.create ~mode:(`TCP (`Port 8000)) (Cohttp_lwt_unix.Server.make ~callback ())
