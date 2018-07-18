eddb1f892d1431268a3746b1456553ec83647858
Tóth Róbert <kkdstryker@gmail.com>
2018-07-18 22:01:37 +0200

    Initial not working implementation of the path variable catcher.

--- path.ml
+++ path.ml
@@ -15,3 +15,7 @@ let anytihng = Re.rep1 (Re.compl [Re.char '/'])
 let any ?(name = "") () = make ~name anytihng
 
 let sep = make (Re.char '/')
+
+let get_path_parts path =
+  let path_separator = Re.compile sep.regex in
+  Re.split path_separator  path
--- request.ml
+++ request.ml
@@ -4,7 +4,7 @@ type t = {
   meth: Cohttp.Code.meth;
   path: string;
   headers: Cohttp.Header.t;
-  uri_parameter: (string * string option) list;
+  path_parameter: (string * string option) list;
   query_parameter: (string * string list) list;
   body: string;
 }
@@ -14,14 +14,29 @@ let make request body =
   let meth = Cohttp.Request.meth request in
   let headers =  Cohttp.Request.headers request in
   let path = Uri.path uri in
-  let uri_parameter = [] in
+  let path_parameter = [] in
   let query_parameter = Uri.query uri in
   Cohttp_lwt.Body.to_string body >>= fun body ->
   Lwt.return {
     meth;
     path;
     headers;
-    uri_parameter;
+    path_parameter;
     query_parameter;
     body;
   }
+
+let check_zip accum _ = accum
+
+let find_path_parameter zipped =
+  Base.List.fold zipped ~init:[] ~f:check_zip
+
+let update_path_parameter request path_parameter =
+  {request with path_parameter = path_parameter;}
+
+let add_path_parameter request path_handler =
+  let path_parts = Path.get_path_parts request.path in
+  let zipped = Base.List.zip path_handler path_parts in
+  let path_parameter_option = Base.Option.map zipped ~f:find_path_parameter in
+  let path_parameter = Base.Option.value path_parameter_option ~default:[] in
+  {request with path_parameter = path_parameter;}
