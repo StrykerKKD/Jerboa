57b6bd320ad895f368af5ba11be771fd97a02e46
Tóth Róbert <kkdstryker@gmail.com>
2018-10-02 22:03:02 +0200

    Refactor out path mapping.

--- jerboa.ml
+++ jerboa.ml
@@ -2,13 +2,11 @@ type middleware = Request.t -> Request.t
 
 type handler = Request.t -> Response.t
 
-type path_handler = Path.t list
-
 type meth = Cohttp.Code.meth
 
 type route_handler = {
   meth: meth;
-  path_handler: path_handler;
+  path_mapping: Path_mapping.t;
   handler: handler;
 }
 
@@ -19,28 +17,17 @@ let filter_handler_config_by_meth meth handler_config =
       route_handler.meth = meth
     )
 
-let path_handler_composer path_catcher accumulator =
-  Path.separator :: path_catcher :: accumulator
-
-let compose_path_handler path_handler =
-  let path_wit_separator = Base.List.fold_right path_handler ~f:path_handler_composer ~init:[] in
-  let regex_wit_separator = Base.List.map path_wit_separator ~f:(fun path -> path.regex) in
-  let beginning_regex = [Re.bol] in
-  let ending_regex = [Re.opt (Re.char '/'); Re.eol] in
-  let final_regex = Base.List.concat [beginning_regex; regex_wit_separator; ending_regex] in
-  Re.seq final_regex
-
 let find_route_handler route handler_config =
   Base.List.find handler_config ~f:(fun route_handler ->
-      let path_handler = route_handler.path_handler in 
-      let composed_path_handler = compose_path_handler path_handler in
+      let path_mapping = route_handler.path_mapping in 
+      let composed_path_handler = Path_mapping.compose_path_regex path_mapping in
       let compiled_path_handler = Re.compile composed_path_handler in
       Re.execp compiled_path_handler route
     )
 
 let handle_route_handler route_handler request =
-  let path_handler = route_handler.path_handler in
-  let request = Request.add_path_parameters request path_handler in
+  let path_mapping = route_handler.path_mapping in
+  let request = Request.add_path_parameters request path_mapping in
   route_handler.handler request
 
 let apply_route_handler route_handler request =
@@ -65,7 +52,7 @@ let server handler_config =
 
 let my_route_handler = {
   meth = `GET;
-  path_handler = [Path.part "hello"; Path.var "name"];
+  path_mapping = [Path.part "hello"; Path.var "name"];
   handler = (fun request -> 
     let open Request in
     let found_name = Base.List.Assoc.find request.path_parameter ~equal:(=) "name" in
---
+++ path_mapping.ml
@@ -0,0 +1,12 @@
+type t = Path.t list
+
+let path_mapping_composer path_part path_accumulator =
+  Path.separator :: path_part :: path_accumulator
+
+let compose_path_regex path_mapping =
+  let path_wit_separator = Base.List.fold_right path_mapping ~f:path_mapping_composer ~init:[] in
+  let regex_wit_separator = Base.List.map path_wit_separator ~f:(fun path -> path.regex) in
+  let beginning_regex = [Re.bol] in
+  let ending_regex = [Re.opt (Re.char '/'); Re.eol] in
+  let final_regex = Base.List.concat [beginning_regex; regex_wit_separator; ending_regex] in
+  Re.seq final_regex
