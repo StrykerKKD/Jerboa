c5d8a824c169f5496e55e0437be23f4a203809f0
Robert Toth <kkdstryker@gmail.com>
2018-10-23 23:13:32 +0200

    Refactoring part with const.

--- lib/path.ml
+++ lib/path.ml
@@ -6,10 +6,10 @@ type t = {
   regex: Re.t
 }
 
-(** [Path.create_part regex] creates a path part, which won't be captured as a path variable:
+(** [Path.create_const regex] creates a path part, which won't be captured as a path variable:
   - regex: regex defines the wanted path part
 *)
-let create_part regex = {
+let create_const regex = {
   name = "";
   regex;
 }
@@ -26,17 +26,17 @@ let create_var name regex = {
 (** [Path.anything] is a regex, which matches anything in the path part.*)
 let anytihng = Re.rep1 (Re.compl [Re.char '/'])
 
-(** [Path.part path_part] creates a simple path part based on:
+(** [Path.const path_part] creates a simple path part based on:
   - path_part: string based path part
 *)
-let part path_part = create_part (Re.str path_part)
+let const path_part = create_const (Re.str path_part)
 
 (** [Path.var name] create a path variable wich will capture the whole path part based on:
   - name: name of the path variable
 *)
 let var name = create_var name anytihng
 
-let separator = create_part (Re.char '/')
+let separator = create_const (Re.char '/')
 
 let split_into_path_parts path =
   let path_separator = Re.compile separator.regex in
